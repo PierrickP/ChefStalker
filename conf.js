84aecb7b908cfc5a99160381fbeee65fc173d061
Pierrick <mulotmail@gmail.com>
2014-04-25 09:25:57 +0200

    Fix configuration Work with ENV instead of unversioned file

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 module.exports = {
 	port: process.env.PORT || 3000,
-	url: 'http://localhost/chef',
+	url: process.env.STALK_URL || 'http://localhost/chef',
 	connect_url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/chefStalker'
 };
